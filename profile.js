a187602158c3395d330d6f9c74c6b86ca1b48dc5
Peter Rydell <peterydell@gmail.com>
2019-11-25 22:33:55 -0800

    updated files

--- profile.js
+++ profile.js
@@ -6,7 +6,8 @@ const pdf = require('dynamic-html-pdf');
 inquirer
   .prompt({
     message: "Enter your GitHub username:",
-    name: "username"
+    name: "username",
+    stars: "stars"
   })
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
@@ -17,25 +18,24 @@ inquirer
       });
 
       const repoNamesStr = repoNames.join("\n");
+    
+    axios.get(queryUrl).then(function(res) {
+      const starStr = res.data.map(function(repo) {
+        return repo.name;
+      });
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+      const starTotalStr = starStr.join("\n");
+
+      fs.writeFile("profile.txt", repoNamesStr, starTotalStr, function(err) {
         if (err) {
           throw err;
         }
 
-        console.log(`Saved ${repoNames.length} repos`);
+        console.log(`Saved ${repoNames.length} repos and ${starStr.length}`);
       });
     });
   });
 
-  // pdf.create(document, options)
+});
 
-  // function writeToFile(fileName, data) {
- 
-  // }
-  
-  // function init() {
-  
-  // init();
-  
-  // }
+  // pdf.create(document, options)
