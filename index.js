44257770ec5938e490da0595b37ad1e7fc118a7a
Peter Rydell <peterydell@gmail.com>
2019-12-15 17:56:31 -0800

    script updates

--- index.js
+++ index.js
@@ -35,50 +35,37 @@ function init() {
     }
   ])
 
-  .then(function({username, color}) {
-    //Calls GitHub API of a user
-    const queryUrl = `https://api.github.com/users/${username}`;
-    return axios.get(queryUrl).then(function(res) {
+  .then(function({ username, color }) {
+    const config = { headers: { accept: "application/json" } };
+    let queryUrl = ` https://api.github.com/users/${username}`;
+    return axios.get(queryUrl, config).then(userData => {
+      let newUrl = `https://api.github.com/users/${username}/starred`;
+    
 
-      //Calls GitHub API for the users stars
-      const starDataUrl = `https://api.github.com/users/${username}/starred`;
-      axios.get(starDataUrl).then(function(stars) {
-
-        if (res.data.bio === null) {
-          res.data.bio = `<h3 class = "userBio">${username}, "does not yet have a Bio on GitHub."</h3>`;
-        }
-        if (res.data.blog === "") {
-          res.data.blog = `<h3 class = "userBlog">${username}, "does not yet have a Blog on GitHub."</h3>`;
-        }
-        if (res.data.location === null) {
-          res.data.location = `<h3 class = "userLocation">${username}, "has not yet added their location on GitHub."</h3>`;
-        }
-
-        //Data object
+      axios.get(newUrl, config).then(starredRepos => {
         data = {
-          img: res.data.avatar_url,
-          username: res.data.login,
-          location: res.data.location,
-          profile: res.data.url,
-          blog: res.data.blog,
-          bio: res.data.bio,
-          repos: res.data.public_repos,
-          followers: res.data.followers,
-          following: res.data.following,
-          stars: stars.data.length,
+          img: userData.data.avatar_url,
+          location: userData.data.location,
+          gitProfile: userData.data.html_url,
+          userBlog: userData.data.blog,
+          userBio: userData.data.bio,
+          repoNum: userData.data.public_repos,
+          followers: userData.data.followers,
+          following: userData.data.following,
+          starNum:starredRepos.data.length,
+          username: username,
           color: color
         };
+         generateHTML(data);
+         writeToHTML(generateHTML(data));
+         makePdf(username);
+         console.log("New profile created successfully!");
 
-
-        generateHTML(data);
-        writeToHTML(generateHTML(data));
-        makePdf(username);
-        console.log("New profile created successfully!");
-
+      });
     });
   });
-});
 }
+
 
 const writeToHTML = function(generateHTML){
   writeFileAsync("index.html", generateHTML);
